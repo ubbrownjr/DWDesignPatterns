6f0ae2416ecc51c5a370192afa3da94b3b909142
pc <pc@UB>
2016-10-29 21:47:21 -0400

    Initial Commit

---
+++ Database/Admin/Stored Procedures/CreateDB_SSISDesignPatterns.sql
@@ -0,0 +1,28 @@
+﻿/*
+ Pre-Deployment Script Template							
+--------------------------------------------------------------------------------------
+ This file contains SQL statements that will be executed before the build script.	
+ Use SQLCMD syntax to include a file in the pre-deployment script.			
+ Example:      :r .\myfile.sql								
+ Use SQLCMD syntax to reference a variable in the pre-deployment script.		
+ Example:      :setvar TableName MyTable							
+               SELECT * FROM [$(TableName)]					
+--------------------------------------------------------------------------------------
+*/
+USE [master];
+GO
+DROP DATABASE IF EXISTS SSISDesignPatterns;
+CREATE DATABASE SSISDesignPatterns
+ON PRIMARY
+( NAME = SSISDPDat,
+    FILENAME = 'C:\MSSQL13.MSSQLSERVER\MSSQL\User\ssisdp.mdf',
+    SIZE = 10MB,
+    MAXSIZE = 50MB,
+    FILEGROWTH = 10% )
+LOG ON
+( NAME = SSISDPLog,
+    FILENAME = 'C:\MSSQL13.MSSQLSERVER\MSSQL\Log\ssisdp.ldf',
+    SIZE = 5MB,
+    MAXSIZE = 25MB,
+    FILEGROWTH = 5MB ) ;
+GO
